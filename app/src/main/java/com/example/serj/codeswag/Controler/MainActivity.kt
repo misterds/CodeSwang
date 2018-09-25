f20a40510df13415c1a3e104b46b1bbf27c49266
misterds <misterds1@mail.ru>
2018-09-25 12:24:30 -0400

    Lesson-02

--- app/src/main/java/com/example/serj/codeswag/Controler/MainActivity.kt
+++ app/src/main/java/com/example/serj/codeswag/Controler/MainActivity.kt
@@ -1,13 +1,32 @@
 package com.example.serj.codeswag.Controler
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
+import com.example.serj.codeswag.Model.Category
 import com.example.serj.codeswag.R
+import com.example.serj.codeswag.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+
+
+//Creating adapter
+    lateinit var adapter :ArrayAdapter<Category>
+//--------------------------------------------------------
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//--------------------------------------------------------
+//initialise  adapter
+
+     adapter = ArrayAdapter(this,
+             android.R.layout.simple_list_item_1,
+             DataService.category)
+
+//setup the list View
+    categoryListView.adapter = adapter
+
     }
 }
--- app/src/main/java/com/example/serj/codeswag/Model/Category.kt
+++ app/src/main/java/com/example/serj/codeswag/Model/Category.kt
@@ -2,4 +2,8 @@ package com.example.serj.codeswag.Model
 
 
 //creating constructors
-class Category(val title:String, val image:String)
+class Category(val title:String, val image:String){
+    override fun toString():String{
+        return title
+    }
+}
